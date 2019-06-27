38cfe05e6689b0c7dd240b226bfefcad845ae4d6
鈴木 <@TSD-SUZUKI>
2019-06-27 13:21:36 +0900

    SQL

---
+++ sql/list.sql
@@ -0,0 +1,16 @@
+
+create table list(
+	list_id int primary key auto_increment,
+	title varchar(50) not null,
+	detail text,
+	importance int(3) default'1',
+	limit_time date
+);
+
+insert into list(title, detail, importance, limit_time) values
+('タイトル1','テスト1',3,'2019/06/01'),
+('タイトル2','テスト2',1,'2019/06/01'),
+('タイトル3','テスト3',2,'2019/06/01');
+
+
+
