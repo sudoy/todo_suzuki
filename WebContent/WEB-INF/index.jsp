03627b8d7dafd7268d8aca731f240f4e6cb03405
鈴木 <@TSD-SUZUKI>
2019-06-27 15:03:32 +0900

    04 index.htmlとDB

--- WebContent/WEB-INF/index.jsp
+++ WebContent/WEB-INF/index.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
 <!--上-->
 <jsp:include page="header.jsp" />
@@ -20,30 +21,14 @@
 				</tr>
 			</thead>
 			<tbody>
+			<c:forEach var="list" items="${list}">
 				<tr>
-					<td>1</td>
-					<td><a href="update.html">テストテスト</a></td>
-					<td>★★★</td>
-					<td>2015/06/20</td>
+					<td>${list.list_id}</td>
+					<td><a href="update.html">${list.title}</a></td>
+					<td>${list.importance}</td>
+					<td>${list.limit_time}</td>
 				</tr>
-				<tr>
-					<td>2</td>
-					<td><a href="update.html">テストテスト</a></td>
-					<td>★</td>
-					<td>2015/06/22</td>
-				</tr>
-				<tr>
-					<td>3</td>
-					<td><a href="update.html">テストテスト</a></td>
-					<td>★★★</td>
-					<td>2015/06/20</td>
-				</tr>
-				<tr>
-					<td>4</td>
-					<td><a href="update.html">テストテスト</a></td>
-					<td>★★</td>
-					<td></td>
-				</tr>
+			</c:forEach>
 			</tbody>
 		</table>
 
---
+++ src/todo/DBUtils.java
@@ -0,0 +1,64 @@
+package todo;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class DBUtils {
+
+	//DB接続
+	public static Connection getConnection() throws NamingException, SQLException {
+		Context initContext = new InitialContext();
+		Context envContext = (Context)initContext.lookup("java:/comp/env");
+		DataSource ds = (DataSource)envContext.lookup("todolist_suzuki");
+		return ds.getConnection();
+
+	}
+
+	//DB閉じる
+	public static void close(Connection c,PreparedStatement p,ResultSet r){
+		try {
+			if(r != null){
+				r.close();
+			}
+			if(p != null){
+				p.close();
+			}
+			if(c != null){
+				c.close();
+			}
+		} catch (Exception e) {}
+	}
+	public static void close(Connection c,PreparedStatement p){
+		try {
+			if(p != null){
+				p.close();
+			}
+			if(c != null){
+				c.close();
+			}
+		} catch (Exception e) {}
+
+	}
+	public static void colse(Connection c){
+		try{
+			if(c != null){ c.close(); }
+		}catch(Exception e) {}
+	}
+	public static void close(PreparedStatement p){
+		try{
+			if(p != null){ p.close(); }
+		}catch(Exception e) {}
+	}
+	public static void close(ResultSet r){
+		try{
+			if(r != null){ r.close(); }
+		}catch(Exception e) {}
+	}
+}
